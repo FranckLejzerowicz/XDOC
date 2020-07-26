5a01d19318ac9f208044f26fadc37850831645bb
FranckLejzerowicz <flejzerowicz@ucsd.edu>
2020-07-25 19:34:48 -0700

    interpolate

--- XDOC/scripts/_standalone_xdoc.py
+++ XDOC/scripts/_standalone_xdoc.py
@@ -74,7 +74,7 @@ from XDOC import __version__
     help="Moving average window to use for estimating where negative slope starts."
 )
 @click.option(
-    "-ci", "--p-ci", required=False, type=tuple, default=(0.025, 0.5, 0.975,),
+    "-ci", "--p-ci", required=False, type=float, default=(0.025, 0.5, 0.975,),
     multiple=True, show_default=True, help="Quantiles for confidence intervals."
 )
 @click.option(
