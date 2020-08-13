b5d1f35cba49d896f29f4155e623e337d0deef4e
FranckLejzerowicz <flejzerowicz@ucsd.edu>
2020-08-13 14:18:56 -0700

    allow use / no use multiprocessing

--- XDOC/ci.py
+++ XDOC/ci.py
@@ -13,36 +13,15 @@ import numpy as np
 def DOC_ci(lowp: pd.DataFrame, p_ci: tuple):
     """
     """
-
-    print("lowp.loc[:5, :]")
-    print(lowp.loc[:5, :])
-    print(lowp.loc[-5:, :])
-
     rjsd = lowp.iloc[:, 1:]
-
-    print("rjsd.loc[:5, :]")
-    print(rjsd.loc[:5, :])
-    print(rjsd.loc[-5:, :])
-
-    print("np.percentile(rjsd, axis=1, q=[x*100 for x in p_ci])")
-    print(np.percentile(rjsd, axis=1, q=[x*100 for x in p_ci]))
-
     cis = pd.DataFrame(
         np.percentile(
             rjsd, axis=1, q=[x*100 for x in p_ci]),
         index=list(p_ci),
         columns=rjsd.index).T
 
-    print("cis.loc[:5, :]")
-    print(cis.loc[:5, :])
-    print(cis.loc[-5:, :])
-
     LCI = pd.concat([pd.DataFrame({'Overlap': lowp.iloc[:, 0].values}), cis],
                     axis=1, sort=False)
     LCI = LCI.loc[~LCI.isna().any(axis=1)]
-
-    print("LCI.loc[:5, :]")
-    print(LCI.loc[:5, :])
-    print(LCI.loc[-5:, :])
 
     return LCI
