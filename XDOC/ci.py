8a11a86133d37af236e269c863d89bdbe0288710
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2023-02-01 21:23:24 +0100

    remove prints

--- XDOC/ci.py
+++ XDOC/ci.py
@@ -13,43 +13,43 @@ import numpy as np
 def DOC_ci(BOOT: pd.DataFrame, p_ci: tuple):
     """
     """
-    print()
-    print()
-    print("BOOT")
-    print(BOOT)
+    # print()
+    # print()
+    # print("BOOT")
+    # print(BOOT)
 
     rjsd = BOOT.iloc[:, 1:]
-    print()
-    print()
-    print("rjsd")
-    print(rjsd)
+    # print()
+    # print()
+    # print("rjsd")
+    # print(rjsd)
 
     cis = pd.DataFrame(
         np.percentile(
             rjsd, axis=1, q=[x*100 for x in p_ci]),
         index=list(p_ci),
         columns=rjsd.index).T
-    print()
-    print()
-    print("cis")
-    print(cis)
+    # print()
+    # print()
+    # print("cis")
+    # print(cis)
 
-    print()
-    print()
-    print("cis.loc[~cis.isna().any(axis=1)]")
-    print(cis.loc[~cis.isna().any(axis=1)])
+    # print()
+    # print()
+    # print("cis.loc[~cis.isna().any(axis=1)]")
+    # print(cis.loc[~cis.isna().any(axis=1)])
 
     LCI = pd.concat([pd.DataFrame({'Overlap': BOOT.iloc[:, 0].values}), cis],
                     axis=1, sort=False)
-    print()
-    print()
-    print("LCI")
-    print(LCI)
+    # print()
+    # print()
+    # print("LCI")
+    # print(LCI)
     LCI = LCI.loc[~LCI.isna().any(axis=1)]
 
-    print()
-    print()
-    print("LCI")
-    print(LCI)
+    # print()
+    # print()
+    # print("LCI")
+    # print(LCI)
 
     return LCI
